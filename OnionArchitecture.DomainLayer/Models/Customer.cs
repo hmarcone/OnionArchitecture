584f90b864b6421a87b991287842cde78d4d5a20
Humberto Marcone <hmarcone@gmail.com>
2021-06-22 19:17:34 -0300

    Implementando a controller na layer de webApi

--- OnionArchitecture.DomainLayer/Models/Customer.cs
+++ OnionArchitecture.DomainLayer/Models/Customer.cs
@@ -1,9 +1,16 @@
-﻿namespace OnionArchitecture.DomainLayer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnionArchitecture.DomainLayer.Models
 {
     public class Customer: BaseEntity
     {
+        //[Required]
         public string CustomerName { get; set; }
+
+        //[Required]
         public string PurchasesProduct { get; set; }
+
+        //[Required]
         public string PaymentType { get; set; }
     }
 }
